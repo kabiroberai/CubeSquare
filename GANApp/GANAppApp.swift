3006785c0fc1245096b4007181c56adac93ac54b
Kabir Oberai <oberai.kabir@gmail.com>
2024-08-04 02:49:16 +0530

    Initial Commit

---
+++ GANApp/GANAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GANAppApp.swift
+//  GANApp
+//
+//  Created by Kabir Oberai on 04/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GANAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GANAppTests/GANAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  GANAppTests.swift
+//  GANAppTests
+//
+//  Created by Kabir Oberai on 04/08/24.
+//
+
+import Testing
+
+struct GANAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
